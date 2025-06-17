b60c27e1cc8948eded251f713c6d333461d2304d
Nikita <belka.nik02@gmail.com>
2025-06-17 13:55:16 +0300

    add toast

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,25 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
         <App />
+        <ToastContainer
+          position='top-right'
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Router>
     </Provider>
   </React.StrictMode>,
